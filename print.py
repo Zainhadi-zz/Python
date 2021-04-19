0822190d3f0009406081aff184a2f33992324852
Zain Hadi Ansari <Zainhadi@users.noreply.github.com>
2021-04-19 19:54:28 +0500

    first practice

---
+++ print.py
@@ -0,0 +1,15 @@
+print("we here",33,"karachi")
+
+print(2+2)
+
+print("2 + 2 = {}".format(2+2))
+
+a = 5
+b = 10
+
+print("{} x {} = {}".format(a,b,a*b))
+#Practice Code
+"""
+Wehere making code easy
+"""
+
