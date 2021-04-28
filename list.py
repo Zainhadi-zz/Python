ce8ac06440237a8f987e258bf7f7c3d08b663bd5
Zain Hadi Ansari <Zainhadi@users.noreply.github.com>
2021-04-28 11:43:06 +0500

    practicing list

---
+++ list.py
@@ -0,0 +1,28 @@
+# list vs array 
+# list is heterogeneous
+# array is homogeneous
+
+data = [1,2,45,35,5,46,464,42,1,31,31,313]
+
+data1 = ["qwrfw","3wfwfw","3fwfw"]
+
+#print(data,"\n",data1)
+
+#print(data1[-1])
+
+
+#print(data[:5])
+
+# list1 = [23,43,[2423,24434,[242324,4,34,34,[34,24,24,24,[24,4234]]]]]
+
+# print(list1[2][2][4][4][1])
+
+
+
+#list2 = [1,2,3,4,5,6,7,8,9,10]
+
+#print(list2[::2])
+#reserve order
+#print(list2[::-1])
+
+#mutable
